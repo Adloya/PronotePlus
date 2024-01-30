f14aa417d1b9a2172fdca7b4529de7840278e7ea
Adloya <34348778+Adloya@users.noreply.github.com>
2024-01-30 22:23:50 +0100

    Thème Abyss - Bouton de colorimétrie des notes

--- Content.js
+++ Content.js
@@ -28,6 +28,20 @@ setTimeout(() => {
       // Dark Theme
       localStorage.setItem("plus-theme", "dark");
       link_rel.href = browser.runtime.getURL("css/dark.css");
+    } else if (theme === "abyss") {
+      localStorage.setItem("plus-theme", "abyss")
+      link_rel.href = browser.runtime.getURL("css/abyss.css")
+    }
+  }
+
+  function patchColor(bool) {
+    if (bool === 'true') {
+      // Enabled
+      localStorage.setItem("colorimetrie", 'true');
+      
+    } else if (bool === 'false') {
+      // Disabled
+      localStorage.setItem("colorimetrie", 'false');
     }
   }
 
@@ -57,7 +71,7 @@ setTimeout(() => {
   if (
     (localStorage.getItem("plus-theme") === null) &
       (localStorage.getItem("plus-theme") !== "dark") &&
-    localStorage.getItem("plus-theme") !== "light"
+    localStorage.getItem("plus-theme") !== "light" && localStorage.getItem("plus-theme" !== "abyss")
   ) {
     // No theme has been set, we set the default one
     console.log("Pas de thème trouvé ! Le thème clair est appliqué.");
@@ -68,8 +82,9 @@ setTimeout(() => {
   } else if (localStorage.getItem("plus-theme") === "dark") {
     // Dark Theme
     patchCSS("dark");
+  } else if (localStorage.getItem ("plus-theme") === "abyss") {
+    patchCSS("abyss")
   }
-
 
   // Creating the theme switcher button element
   let themeSwitcher = document.createElement("a");
@@ -79,23 +94,50 @@ setTimeout(() => {
   themeSwitcher.innerHTML =
     '<svg id="tsSVG"xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="0 0 576 512"><path style="color: #8a8a8a;" d="M342.7 352.7c5.75-9.625 9.25-20.75 9.25-32.75c0-35.25-28.75-64-63.1-64c-17.25 0-32.75 6.875-44.25 17.87C227.4 244.2 196.2 223.1 159.1 223.1c-53 0-96 43.06-96 96.06c0 2 .5029 3.687 .6279 5.687c-37.5 13-64.62 48.38-64.62 90.25C-.0048 468.1 42.99 512 95.99 512h239.1c44.25 0 79.1-35.75 79.1-80C415.1 390.1 383.7 356.2 342.7 352.7zM565.2 298.4c-93 17.75-178.5-53.62-178.5-147.6c0-54.25 28.1-104 76.12-130.9c7.375-4.125 5.375-15.12-2.75-16.63C448.4 1.125 436.7 0 424.1 0c-105.9 0-191.9 85.88-191.9 192c0 8.5 .625 16.75 1.75 25c5.875 4.25 11.62 8.875 16.75 14.25C262.1 226.5 275.2 224 287.1 224c52.88 0 95.1 43.13 95.1 96c0 3.625-.25 7.25-.625 10.75c23.62 10.75 42.37 29.5 53.5 52.5c54.38-3.375 103.7-29.25 137.1-70.37C579.2 306.4 573.5 296.8 565.2 298.4z"/></svg>';
   
+  // Creating the colorimetry of the notes button element
+  let colorSwitcher = document.createElement("a");
+  colorSwitcher.id = "color-switcher";
+  colorSwitcher.href = "#color-switched";
+  colorSwitcher.title = "Activer / Désactiver la colorimétrie des notes";
+  colorSwitcher.innerHTML =
+    '<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet"><g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#8a8a8a" stroke="none"><path d="M1951 5109 c-46 -18 -84 -53 -102 -93 -18 -40 -19 -122 -19 -2456 0 -2351 1 -2416 19 -2457 12 -26 35 -52 62 -70 l43 -28 606 0 606 0 41 27 c24 16 49 44 62 70 l21 44 0 2415 c0 2350 -1 2415 -19 2456 -12 26 -35 52 -62 70 l-43 28 -595 2 c-361 1 -605 -2 -620 -8z"/><path d="M3794 3650 c-47 -11 -92 -47 -114 -90 -20 -39 -20 -65 -20 -1730 0 -1663 0 -1691 20 -1729 23 -47 60 -77 109 -92 24 -7 233 -9 622 -7 l585 3 41 27 c24 16 49 44 62 70 l21 44 0 1682 c0 1391 -2 1688 -14 1715 -17 43 -76 93 -123 106 -44 13 -1139 14 -1189 1z"/><path d="M125 3099 c-38 -11 -94 -64 -111 -106 -12 -27 -14 -280 -14 -1440 l0 -1407 21 -44 c13 -26 38 -54 62 -70 l41 -27 585 -3 c389 -2 598 0 622 7 49 15 86 45 109 92 20 37 20 65 20 1454 0 1370 -1 1417 -19 1453 -23 46 -46 67 -94 87 -31 13 -115 15 -614 14 -318 0 -592 -5 -608 -10z"/></g></svg>';
+  
   // Listening for a "click" event
   themeSwitcher.addEventListener("click", function (e) {
     e.preventDefault();
     if (localStorage.getItem("plus-theme") === "light") {
       patchCSS("dark");
     } else if (localStorage.getItem("plus-theme") === "dark") {
+      patchCSS("abyss");
+    } else if (localStorage.getItem("plus-theme") === "abyss"){
       patchCSS("light");
-    } else {
-      patchCSS("light");
+    }else{
+      patchCSS("light")
     }
   });
+
+  colorSwitcher.addEventListener('click', function (e) {
+    e.preventDefault();
+    if(localStorage.getItem("colorimetrie") === 'true') {
+      patchColor('false')
+    }else if(localStorage.getItem("colorimetrie") === 'false') {
+      patchColor('true')
+    }else {
+      patchColor('false')
+    }
+  })
 
   // Append the button. Patched the double button bug
   if(!document.getElementById("theme-switcher")){
     document
     .getElementsByClassName("menu-commandes")[0]
     .appendChild(themeSwitcher);
+  }
+
+  if(!document.getElementById("color-switcher")){
+    document
+    .getElementsByClassName("menu-commandes")[0]
+    .appendChild(colorSwitcher);
   }
 
   // * Part 2 of init : Loop page patching.
@@ -112,45 +154,68 @@ setTimeout(() => {
 
         // Note patching
         document.querySelectorAll(".as-info").forEach((note) => {
-          if (note.style.borderRadius !== "5px") {
-            // Checking if it is already patched
-            note.style.borderRadius = "5px";
-            note.style.fontSize = "12px";
-            if (note.innerText.includes("/")) {
-              const noteDonnee = parseFloat(note.innerText.split("/")[0], 10);
-              const bareme = parseFloat(note.innerText.split("/")[1], 10);
-              const convertedNote = (noteDonnee / bareme) * 20;
-
-              console.log(convertedNote)
-              if (convertedNote < 10) {
-                note.style.color = "#ff6161";
-              } else if (convertedNote >= 15) {
-                note.style.backgroundColor = "#28ad37";
-                note.style.color = "white";
-              } else if (convertedNote > 13) {
-                note.style.backgroundColor = "#55c974";
-                note.style.color = "white";
-              } else if (convertedNote < 13) {
-                note.style.backgroundColor = "#ffd061";
-                note.style.color = "black";
+          if(localStorage.getItem("colorimetrie") === 'false') {
+            if (note.style.borderRadius !== "5px") {
+              // Checking if it is already patched
+              if (note.innerText.includes("/")) {
+                const noteDonnee = parseFloat(note.innerText.split("/")[0], 10);
+                const bareme = parseFloat(note.innerText.split("/")[1], 10);
+                const convertedNote = (noteDonnee / bareme) * 20;
+                note.style.borderRadius = "5px";
+                note.style.fontSize = "12px";
+  
+                if (convertedNote < 10) {
+                  note.style.backgroundColor = "var(--notebg-red)";
+                  note.style.border = "var(--noteborder-red)"
+                  note.style.color = "var(--notetext)";
+                } else if (convertedNote >= 15) {
+                  note.style.backgroundColor = "var(--notebg-dg)";
+                  note.style.border = "var(--noteborder-dg)"
+                  note.style.color = "var(--notetext)";
+                } else if (convertedNote > 13) {
+                  ote.style.backgroundColor = "var(--notebg-lg)";
+                  note.style.border = "var(--noteborder-lg)"
+                  note.style.color = "var(--notetext)";
+                } else if (convertedNote < 13) {
+                  note.style.backgroundColor = "var(--notebg-y)";
+                  note.style.border = "var(--noteborder-y)"
+                  note.style.color = "var(--notetext)";
+                }
+                if(!note.innerHTML.includes("<br>")) {
+                  note.innerHTML = `${note.innerHTML}<br>( ${convertedNote}<span class="bareme"> /20</span> )`;
+                }
+              } else {
+                note.style.borderRadius = "5px";
+                note.style.fontSize = "12px";
+                const noteDonnee = parseFloat(note.innerText, 10);
+                if(isNaN(noteDonnee)) {
+                  note.style.backgroundColor = "var(--notebg-gray)";
+                  note.style.border = "var(--noteborder-gray)"
+                  note.style.color = "var(--notetext)";
+                }
+                if(note.innerHTML.includes("bareme")) {
+                  note.innerHTML = `${note.innerText}<span class="bareme"> /20</span>`;
+                }
+                if (noteDonnee < 10) {
+                  note.style.color = "#ff6161";
+                } else if (noteDonnee >= 15) {
+                  note.style.backgroundColor = "var(--notebg-dg)";
+                  note.style.border = "var(--noteborder-dg)"
+                  note.style.color = "var(--notetext)";
+                } else if (noteDonnee > 13) {
+                  note.style.backgroundColor = "var(--notebg-lg)";
+                  note.style.border = "var(--noteborder-lg)"
+                  note.style.color = "var(--notetext)";
+                } else if (noteDonnee < 13) {
+                  note.style.backgroundColor = "var(--notebg-y)";
+                  note.style.border = "var(--noteborder-y)"
+                  note.style.color = "var(--notetext)";
+                }
               }
-              note.innerHTML = `${note.innerHTML}<br>( ${convertedNote}<span class="bareme"> /20</span> )`;
-            } else {
-              const noteDonnee = parseFloat(note.innerText, 10);
-              note.innerHTML = `${note.innerText}<span class="bareme"> /20</span>`;
-              if (noteDonnee < 10) {
-                note.style.color = "#ff6161";
-              } else if (noteDonnee >= 15) {
-                note.style.backgroundColor = "#28ad37";
-                note.style.color = "white";
-              } else if (noteDonnee > 13) {
-                note.style.backgroundColor = "#55c974";
-                note.style.color = "white";
-              } else if (noteDonnee < 13) {
-                note.style.backgroundColor = "#ffd061";
-                note.style.color = "white";
-              }
-            }
+          } 
+          }else{
+            note.style = ""
+            note.innerText = note.innerText
           }
         });
       }
@@ -179,8 +244,6 @@ setTimeout(() => {
                                 <br>
                                 - Adresse Postale : Index Éducation CS 90001 13388 Marseille CEDEX 13
                                 <br>
-                                - Télécopie : +33 (0)4 96 15 00 06
-                                <br>
                                 - Téléphone : <a href="tel:"+33496152170">+33 (0)4 96 15 21 70</a> (Appel non surtaxé)
                                 <br>
                                 - Contacter par <a href="https://www.index-education.com/fr/assistance-email.php" target="_BLANK">Email</a>
@@ -196,9 +259,8 @@ setTimeout(() => {
                             <br>
                             - Application d'aide contre le cyberharcèlement : <a href="https://apps.apple.com/us/app/3018/id1602242493" target="_BLANK">IOS</a> | <a href="https://play.google.com/store/apps/details?id=org.eenfance.android.app3018" target="_BLANK">ANDROID</a>
                             <br>
-                            - En cas d'envies suicidaires (SOS Amitié) : <a href="tel:"0972394050">09 72 39 40 50</a> <strong>(Prix d'un appel local)</strong>
+                            - En cas de pensées suicidaires ou autodestructrices, des numéros peuvent vous aider : (SOS Amitié) : <a href="tel:"0972394050">09 72 39 40 50</a> <strong>(Prix d'un appel local)</strong>
                         </span>
-                        <br>
                         <br>
                     </div>
                     <br>
@@ -207,13 +269,10 @@ setTimeout(() => {
                         <span>
                             <strong>A propos de Pronote+ (Firefox)</strong>
                             <br>
-                            -👨‍💻Développé et maintenu par <strong><a href="https://github.com/Adloya" target="_BLANK">Adloya</a></strong>
+                            -👨‍💻Développé et maintenu par <strong><a href="https://github.com/Adloya" target="_BLANK">Adloya</a> </strong> et <strong><a href="https://github.com/Malolamgo" target="_BLANK">Malolam21</a> </strong>
                             <br>
                             - 🖇 Liens : <a href="https://github.com/Adloya/PronotePlus">Github</a> | <a href="https://addons.mozilla.org/fr/firefox/addon/pronoteplus/">Firefox page</a>
-                            <br>
-                            - ⭐ Remerciment spécial à : Malolam21
                         </span>
-                        <br>
                         <br>
                     </div>
                     `;
