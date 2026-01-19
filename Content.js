5601b66dd4c843fe41e74f748e8e541fb5b17d29
Adloya <34348778+Adloya@users.noreply.github.com>
2026-01-19 22:23:37 +0100

    0.2.6 - Services externes

--- Content.js
+++ Content.js
@@ -11,7 +11,7 @@ setTimeout(() => {
   pglink.rel = "stylesheet";
   pglink.id = "pagetheme-link";
 
-  localStorage.setItem("pplus-current-version", "0.2.4")
+  localStorage.setItem("pplus-current-version", "0.2.4");
 
   document.getElementsByTagName("head")[0].appendChild(link);
 
@@ -31,19 +31,18 @@ setTimeout(() => {
       localStorage.setItem("plus-theme", "dark");
       link_rel.href = browser.runtime.getURL("css/dark.css");
     } else if (theme === "abyss") {
-      localStorage.setItem("plus-theme", "abyss")
-      link_rel.href = browser.runtime.getURL("css/abyss.css")
+      localStorage.setItem("plus-theme", "abyss");
+      link_rel.href = browser.runtime.getURL("css/abyss.css");
     }
   }
 
   function patchColor(bool) {
-    if (bool === 'true') {
+    if (bool === "true") {
       // Enabled
-      localStorage.setItem("colorimetrie", 'true');
-      
-    } else if (bool === 'false') {
+      localStorage.setItem("colorimetrie", "true");
+    } else if (bool === "false") {
       // Disabled
-      localStorage.setItem("colorimetrie", 'false');
+      localStorage.setItem("colorimetrie", "false");
     }
   }
 
@@ -73,7 +72,8 @@ setTimeout(() => {
   if (
     (localStorage.getItem("plus-theme") === null) &
       (localStorage.getItem("plus-theme") !== "dark") &&
-    localStorage.getItem("plus-theme") !== "light" && localStorage.getItem("plus-theme" !== "abyss")
+    localStorage.getItem("plus-theme") !== "light" &&
+    localStorage.getItem("plus-theme" !== "abyss")
   ) {
     // No theme has been set, we set the default one
     console.log("Pas de thème trouvé ! Le thème clair est appliqué.");
@@ -84,8 +84,8 @@ setTimeout(() => {
   } else if (localStorage.getItem("plus-theme") === "dark") {
     // Dark Theme
     patchCSS("dark");
-  } else if (localStorage.getItem ("plus-theme") === "abyss") {
-    patchCSS("abyss")
+  } else if (localStorage.getItem("plus-theme") === "abyss") {
+    patchCSS("abyss");
   }
 
   // Creating the theme switcher button element
@@ -95,7 +95,7 @@ setTimeout(() => {
   themeSwitcher.title = "Changer de thème";
   themeSwitcher.innerHTML =
     '<svg id="tsSVG"xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="0 0 576 512"><path style="color: #8a8a8a;" d="M342.7 352.7c5.75-9.625 9.25-20.75 9.25-32.75c0-35.25-28.75-64-63.1-64c-17.25 0-32.75 6.875-44.25 17.87C227.4 244.2 196.2 223.1 159.1 223.1c-53 0-96 43.06-96 96.06c0 2 .5029 3.687 .6279 5.687c-37.5 13-64.62 48.38-64.62 90.25C-.0048 468.1 42.99 512 95.99 512h239.1c44.25 0 79.1-35.75 79.1-80C415.1 390.1 383.7 356.2 342.7 352.7zM565.2 298.4c-93 17.75-178.5-53.62-178.5-147.6c0-54.25 28.1-104 76.12-130.9c7.375-4.125 5.375-15.12-2.75-16.63C448.4 1.125 436.7 0 424.1 0c-105.9 0-191.9 85.88-191.9 192c0 8.5 .625 16.75 1.75 25c5.875 4.25 11.62 8.875 16.75 14.25C262.1 226.5 275.2 224 287.1 224c52.88 0 95.1 43.13 95.1 96c0 3.625-.25 7.25-.625 10.75c23.62 10.75 42.37 29.5 53.5 52.5c54.38-3.375 103.7-29.25 137.1-70.37C579.2 306.4 573.5 296.8 565.2 298.4z"/></svg>';
-  
+
   // Creating the colorimetry of the notes button element
   let colorSwitcher = document.createElement("a");
   colorSwitcher.id = "color-switcher";
@@ -103,7 +103,7 @@ setTimeout(() => {
   colorSwitcher.title = "Activer / Désactiver la colorimétrie des notes";
   colorSwitcher.innerHTML =
     '<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet"><g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#8a8a8a" stroke="none"><path d="M1951 5109 c-46 -18 -84 -53 -102 -93 -18 -40 -19 -122 -19 -2456 0 -2351 1 -2416 19 -2457 12 -26 35 -52 62 -70 l43 -28 606 0 606 0 41 27 c24 16 49 44 62 70 l21 44 0 2415 c0 2350 -1 2415 -19 2456 -12 26 -35 52 -62 70 l-43 28 -595 2 c-361 1 -605 -2 -620 -8z"/><path d="M3794 3650 c-47 -11 -92 -47 -114 -90 -20 -39 -20 -65 -20 -1730 0 -1663 0 -1691 20 -1729 23 -47 60 -77 109 -92 24 -7 233 -9 622 -7 l585 3 41 27 c24 16 49 44 62 70 l21 44 0 1682 c0 1391 -2 1688 -14 1715 -17 43 -76 93 -123 106 -44 13 -1139 14 -1189 1z"/><path d="M125 3099 c-38 -11 -94 -64 -111 -106 -12 -27 -14 -280 -14 -1440 l0 -1407 21 -44 c13 -26 38 -54 62 -70 l41 -27 585 -3 c389 -2 598 0 622 7 49 15 86 45 109 92 20 37 20 65 20 1454 0 1370 -1 1417 -19 1453 -23 46 -46 67 -94 87 -31 13 -115 15 -614 14 -318 0 -592 -5 -608 -10z"/></g></svg>';
-  
+
   // Listening for a "click" event
   themeSwitcher.addEventListener("click", function (e) {
     e.preventDefault();
@@ -111,35 +111,35 @@ setTimeout(() => {
       patchCSS("dark");
     } else if (localStorage.getItem("plus-theme") === "dark") {
       patchCSS("abyss");
-    } else if (localStorage.getItem("plus-theme") === "abyss"){
+    } else if (localStorage.getItem("plus-theme") === "abyss") {
       patchCSS("light");
-    }else{
-      patchCSS("light")
+    } else {
+      patchCSS("light");
     }
   });
 
-  colorSwitcher.addEventListener('click', function (e) {
+  colorSwitcher.addEventListener("click", function (e) {
     e.preventDefault();
-    if(localStorage.getItem("colorimetrie") === 'true') {
-      patchColor('false')
-    }else if(localStorage.getItem("colorimetrie") === 'false') {
-      patchColor('true')
-    }else {
-      patchColor('false')
+    if (localStorage.getItem("colorimetrie") === "true") {
+      patchColor("false");
+    } else if (localStorage.getItem("colorimetrie") === "false") {
+      patchColor("true");
+    } else {
+      patchColor("false");
     }
-  })
+  });
 
   // Append the button. Patched the double button bug
-  if(!document.getElementById("theme-switcher")){
+  if (!document.getElementById("theme-switcher")) {
     document
-    .getElementsByClassName("menu-commandes")[0]
-    .appendChild(themeSwitcher);
+      .getElementsByClassName("menu-commandes")[0]
+      .appendChild(themeSwitcher);
   }
 
-  if(!document.getElementById("color-switcher")){
+  if (!document.getElementById("color-switcher")) {
     document
-    .getElementsByClassName("menu-commandes")[0]
-    .appendChild(colorSwitcher);
+      .getElementsByClassName("menu-commandes")[0]
+      .appendChild(colorSwitcher);
   }
 
   // * Part 2 of init : Loop page patching.
@@ -156,70 +156,163 @@ setTimeout(() => {
 
         // Note patching
         document.querySelectorAll(".as-info").forEach((note) => {
-          if(localStorage.getItem("colorimetrie") === 'false') {
+          if (localStorage.getItem("colorimetrie") === "false") {
             if (note.style.borderRadius !== "5px") {
               // Checking if it is already patched
               if (note.innerText.includes("/")) {
-                const noteDonnee = parseFloat(note.innerText.replace(",", ".").split("/")[0], 10);
+                const noteDonnee = parseFloat(
+                  note.innerText.replace(",", ".").split("/")[0],
+                  10,
+                );
                 const bareme = parseFloat(note.innerText.split("/")[1], 10);
                 const convertedNote = parseFloat((noteDonnee / bareme) * 20);
                 note.style.borderRadius = "5px";
                 note.style.fontSize = "12px";
-  
+
                 if (convertedNote < 10) {
                   note.style.backgroundColor = "var(--notebg-red)";
-                  note.style.border = "var(--noteborder-red)"
+                  note.style.border = "var(--noteborder-red)";
                   note.style.color = "var(--notetext)";
                 } else if (convertedNote >= 15) {
                   note.style.backgroundColor = "var(--notebg-dg)";
-                  note.style.border = "var(--noteborder-dg)"
+                  note.style.border = "var(--noteborder-dg)";
                   note.style.color = "var(--notetext)";
                 } else if (convertedNote > 13) {
                   ote.style.backgroundColor = "var(--notebg-lg)";
-                  note.style.border = "var(--noteborder-lg)"
+                  note.style.border = "var(--noteborder-lg)";
                   note.style.color = "var(--notetext)";
                 } else if (convertedNote < 13) {
                   note.style.backgroundColor = "var(--notebg-y)";
-                  note.style.border = "var(--noteborder-y)"
+                  note.style.border = "var(--noteborder-y)";
                   note.style.color = "var(--notetext)";
                 }
-                if(!note.innerHTML.includes("<br>")) {
+                if (!note.innerHTML.includes("<br>")) {
                   note.innerHTML = `${note.innerHTML}<br>( ${convertedNote}<span class="bareme"> /20</span> )`;
                 }
               } else {
                 note.style.borderRadius = "5px";
                 note.style.fontSize = "12px";
                 const noteDonnee = parseFloat(note.innerText, 10);
-                if(isNaN(noteDonnee)) {
+                if (isNaN(noteDonnee)) {
                   note.style.backgroundColor = "var(--notebg-gray)";
-                  note.style.border = "var(--noteborder-gray)"
+                  note.style.border = "var(--noteborder-gray)";
                   note.style.color = "var(--notetext)";
                 }
-                if(note.innerHTML.includes("bareme")) {
+                if (note.innerHTML.includes("bareme")) {
                   note.innerHTML = `${note.innerText}<span class="bareme"> /20</span>`;
                 }
                 if (noteDonnee < 10) {
                   note.style.color = "#ff6161";
                 } else if (noteDonnee >= 15) {
                   note.style.backgroundColor = "var(--notebg-dg)";
-                  note.style.border = "var(--noteborder-dg)"
+                  note.style.border = "var(--noteborder-dg)";
                   note.style.color = "var(--notetext)";
                 } else if (noteDonnee > 13) {
                   note.style.backgroundColor = "var(--notebg-lg)";
-                  note.style.border = "var(--noteborder-lg)"
+                  note.style.border = "var(--noteborder-lg)";
                   note.style.color = "var(--notetext)";
                 } else if (noteDonnee < 13) {
                   note.style.backgroundColor = "var(--notebg-y)";
-                  note.style.border = "var(--noteborder-y)"
+                  note.style.border = "var(--noteborder-y)";
                   note.style.color = "var(--notetext)";
                 }
               }
-          } 
-          }else{
-            note.style = ""
-            note.innerText = note.innerText
+            }
+          } else {
+            note.style = "";
+            note.innerText = note.innerText;
           }
         });
+
+        let racext = document.createElement("section");
+        racext.classList.add("widget", "discussions", "collapsible");
+        racext.id = "id_84id_40";
+
+        let racext_head = document.createElement("header");
+        let racext_div = document.createElement("div");
+
+        racext_head.title = "Services externes";
+        racext_head.class = "clickable";
+        racext_head.innerHTML =
+          '<h2 class="clickable" style="cursor: default !important;"><span id="id_84_TitreText" tabindex="0" role="link" data-tooltip="default">Services Externes</span></h2>';
+
+        racext_div.id = "id_84id41";
+        racext_div.classList.add("content-container", "overflow-auto");
+        racext_div.role = "auto";
+
+        if (!document.getElementById("id_84_TitreText"))
+          racext.appendChild(racext_head);
+
+        if (!document.getElementById("id_84id41"))
+          racext.appendChild(racext_div);
+
+        let idetab = window.location.href
+          .split("identifiant")[0]
+          .replace("https://", "")
+          .replace(".index-education.net/pronote/eleve.html?", "");
+
+        let liens_services = {
+          "E-Sidoc": "https://" + idetab + ".esidoc.fr/",
+          Cyclades:
+            "https://candidat.examens-concours.gouv.fr/cyccandidat/portal/login",
+          Onisep: "https://onisep.fr/",
+          Parcoursup: "https://parcoursup.gouv.fr/",
+          "Scolarité Services": "https://teleservices.education.gouv.fr/eds/",
+        };
+
+        let logos_services = {
+          "E-Sidoc":
+            "https://www.sainte-therese-les-cordeliers.fr/wp-content/uploads/2022/04/esidoc-icone.png",
+          Cyclades:
+            "https://candidat.examens-concours.gouv.fr/cyccandidat/resources/images/favicon.ico",
+          Onisep:
+            "https://yt3.googleusercontent.com/xTe_JZ9MGButzL-omLCCefu1Iqj_nF0Qn6CcrdYI0z5O68M50RJT4EWdDdBjGQWiL27UFLVayA=s900-c-k-c0x00ffffff-no-rj",
+          "Scolarité Services":
+            "https://educonnect.education.gouv.fr/idp-static/favicons/favicon.ico",
+          Parcoursup:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSff_3OW-rRSMf-rNZ8wvDLBDv5m6vbfqcpsQ&s",
+        };
+
+        let exp_service = {
+          "E-Sidoc": "Portail numérique des CDI",
+          Cyclades: "Portail des examens et concours",
+          Onisep: "Centre d'information à l'orientation",
+          Parcoursup: "Plateforme d'accès à l'enseignement supérieur",
+          "Scolarité Services": "Services administratifs",
+        };
+
+        let div_int = document.createElement("div");
+        div_int.id = "id_84";
+
+        let divint = document.createElement("ul");
+        divint.style = "list-style-type: none !important;";
+        divint.className = "liste_clickable";
+
+        for (let key in liens_services) {
+          if (liens_services.hasOwnProperty(key)) {
+            if (!document.getElementById(liens_services[key])) {
+              // key = nom du service
+              // liens_services[key] = lien du service
+              divint.innerHTML =
+                divint.innerHTML +
+                `<li id="IE.Identite.collection.g5_notes_0" style="list-style-type: none !important;"><a class="justify-between Color_RecapVS_Lue wrapper-link" href="${liens_services[key]}" target="_BLANK" style="text-decoration: none; z-index: 1000;">
+                  <img height="50" width="50" src="${logos_services[key]}"></img>
+                  <strong>${key}</strong>
+                  <span class="date">${exp_service[key]}</span>
+                </a></li>`;
+            }
+          }
+        }
+
+        div_int.appendChild(divint);
+
+        racext_div.appendChild(div_int);
+
+        let col3 = document.getElementById(
+          "GInterface\.Instances\[2\]_colonne_0",
+        );
+
+        if (!document.getElementById("id_84id_40")) col3.appendChild(racext);
       }
 
       // Account patch
@@ -288,84 +381,6 @@ setTimeout(() => {
 
           image.src = image.getAttribute("data-src") + "&reload=" + Date.now();
         }
-      }
-
-      // Documents patch
-      if (
-        document.getElementsByClassName("objetBandeauEntete_thirdmenu")[0]
-          .children[0].innerText === "Je télécharge mes documents"
-      ) {
-        patchPageCSS("nopage");
-      }
-
-      // Resources patch
-      if (
-        document.getElementsByClassName("conteneur-ressourcePeda").length > 0
-      ) {
-        if (!document.getElementsByClassName("icon_time").length > 0) {
-          patchPageCSS("nopage");
-        }
-      }
-
-      // Homework patch
-      if (document.getElementsByClassName("icon_time").length > 0) {
-        if (!document.getElementsByClassName("icon_asterisk").length > 0) {
-          patchPageCSS("nopage");
-        }
-      }
-
-      // EDT/Calendar patch
-      if (document.getElementsByClassName("Calendrier_Boutons").length > 0) {
-        patchPageCSS("nopage");
-
-        let pasts = document.getElementsByClassName("PastilleRessource");
-        // let etiq = document.getElementsByClassName("EtiquetteCours");
-
-        // for (let i = 0; i < etiq.length; i++) {
-        //   if(!etiq.length === 0) {
-        //     let e = etiq[i].style
-        //     console.log(e)
-        //     if(toString(etiq[i].style).includes("blue")) {
-        //       etiq[i].style = "color: blue !important;"
-        //     }else if(toString(etiq[i].style).includes("rgb(192, 0, 0)")) {
-        //       etiq[i].style.color = "color: rgb(192, 0, 0) !important;"
-        //     }
-        //   }
-        // }
-
-        for (let i = 0; i < pasts.length; i++) {
-          pasts[i].style = "" 
-        }
-      }
-
-      // Messages patch
-      if (
-        document.getElementsByClassName("ilm-listeEtiqu-deploiement").length > 0
-      ) {
-        patchPageCSS("nopage");
-      }
-
-      // Recap patch
-      if (document.getElementsByClassName("icon_time").length > 0) {
-        if (document.getElementsByClassName("icon_asterisk").length > 0) {
-          patchPageCSS("nopage");
-        }
-      }
-
-      // Notes patch
-      if (
-        document.getElementsByClassName("DonneesListe_DernieresNotes").length >
-        0
-      ) {
-        patchPageCSS("nopage");
-        
-      }
-
-      // Comp patch
-      if (
-        document.getElementsByClassName("icon_competence_absent").length > 0
-      ) {
-        patchPageCSS("nopage");
       }
     }
 
